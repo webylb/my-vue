e63121073ba92d8fb3b401bd09e1bf9c7a38d420
yangliangbo <yangliangbo@pinpianyi.com>
2020-11-26 16:47:59 +0800

    :art:

---
+++ src/axios/index.ts
@@ -0,0 +1,18 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:3001'
+
+axios.interceptors.request.use((config: AxiosRequestConfig) => {
+  return config
+})
+
+axios.interceptors.response.use((response: AxiosResponse) => {
+  if(response.data.err === 1) {
+    return Promise.reject(response.data.data)
+  }
+  return response.data.data
+}, err => {
+  return Promise.reject(err)
+})
+
+export default axios
---
+++ src/views/About.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>{{ count }} {{ object.foo }}</div>
+</template>
+
+<script>
+import { ref, reactive, watchEffect } from "vue";
+
+export default {
+  props: {
+    name: {
+      type: String,
+      default: "007"
+    }
+  },
+  setup(props, context) {
+    console.log(props)
+    console.log(context)
+
+    watchEffect(() => {
+      console.log(`name is:` + props.name)
+    })
+
+    const count = ref(0)
+    const object = reactive({ foo: "bar" })
+
+    // 暴露给模板
+    return {
+      count,
+      object
+    }
+  }
+};
+</script>
